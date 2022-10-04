58ea67936d5e500361fb652692a3c114ec0e5cce
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2022-10-03 22:21:22 -0500

    websockets implemeatation

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMessageDto } from './dto/create-message.dto';
-import { UpdateMessageDto } from './dto/update-message.dto';
+import { Message } from './entities/message.entity';
 
 @Injectable()
 export class MessagesService {
+  messages: Message[] = [{ name: 'Hector', text: 'Hello' }];
+  clientToUser = {};
   create(createMessageDto: CreateMessageDto) {
-    return 'This action adds a new message';
+    const message = { ...createMessageDto };
+    this.messages.push(message);
+    return message;
   }
 
   findAll() {
-    return `This action returns all messages`;
+    return this.messages;
+  }
+  join(name: string, clientId: string) {
+    this.clientToUser[clientId] = name;
+    return Object.values(this.clientToUser);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} message`;
-  }
-
-  update(id: number, updateMessageDto: UpdateMessageDto) {
-    return `This action updates a #${id} message`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} message`;
+  getClientName(clientId: string) {
+    return this.clientToUser[clientId];
   }
 }
